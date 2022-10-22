30827cfacfd21e3a0d624a4855b4fc8caaef0dd7
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-10-22 03:43:58 -0400

    HashMap Attempt

--- trying/tryingA.erl
+++ trying/tryingA.erl
@@ -1,1 +1,11 @@
 -module(tryingA).
+-export([main/0]).
+
+main() ->
+    X = map_attempt(),
+    io:fwrite("Map after put(): ~p\n", [X]).
+
+map_attempt() ->
+    Map1 = #{key1 => 1, val1 => 'Actor_PID'},
+    io:fwrite("Map before put(): ~p\n", [Map1]),
+    maps:put(key2, "Actor2_PID", Map1).
