f0c645c6bbe9ffe81f5071b55ee4c1f262d06ca1
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-10-22 11:20:25 -0400

    Sha1 and Modulo

--- trying/tryingA.erl
+++ trying/tryingA.erl
@@ -1,10 +1,12 @@
 -module(tryingA).
+% -import(crypto, []).
 -export([main/0]).
 
 main() ->
     X = map_attempt(),
     io:fwrite("Map after put(): ~p\n", [X]),
-    intToList().
+    intToList(),
+    sha_attempt().
 
 map_attempt() ->
     Map1 = #{key1 => 1, val1 => 'Actor_PID'},
@@ -13,6 +15,15 @@ map_attempt() ->
 
 intToList() ->
     H = integer_to_list(30, 16),
-    io:fwrite("int to hex: ~p\n", [H]),
+    io:fwrite("Int to hex: ~p\n", [H]),
     M = list_to_integer(H, 16),
-    io:fwrite("list to integer: ~p\n", [M]).
+    io:fwrite("List to integer: ~p\n", [M]).
+
+sha_attempt() ->
+    <<Mac:160/integer>> = crypto:hash(sha, "Test String"),
+    Y = lists:flatten(io_lib:format("~40.16.0b", [Mac])),
+    io:fwrite("Sha string: ~p\n", [Y]),
+    M = list_to_integer(Y, 16),
+    io:fwrite("List to integer sha: ~p\n", [M]),
+    Modulo = M rem 127,
+    io:fwrite("Modulo: ~p\n", [Modulo]).
