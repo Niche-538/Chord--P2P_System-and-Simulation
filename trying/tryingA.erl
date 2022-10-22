aefcbf95d375e453c6243c1ad852ca686a71704d
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-10-22 03:51:20 -0400

    Int to Hex And Hex to Int

--- trying/tryingA.erl
+++ trying/tryingA.erl
@@ -3,9 +3,16 @@
 
 main() ->
     X = map_attempt(),
-    io:fwrite("Map after put(): ~p\n", [X]).
+    io:fwrite("Map after put(): ~p\n", [X]),
+    intToList().
 
 map_attempt() ->
     Map1 = #{key1 => 1, val1 => 'Actor_PID'},
     io:fwrite("Map before put(): ~p\n", [Map1]),
     maps:put(key2, "Actor2_PID", Map1).
+
+intToList() ->
+    H = integer_to_list(30, 16),
+    io:fwrite("int to hex: ~p\n", [H]),
+    M = list_to_integer(H, 16),
+    io:fwrite("list to integer: ~p\n", [M]).
